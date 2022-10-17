d40da42f90395c58efa1eb5432f588011c66f1a6
pokealimit <pokealimit@gmail.com>
2022-10-17 11:45:41 +0800

    added ntu config for nanyanglink_2022-02-03.bag

--- docker/run_wslg.sh
+++ docker/run_wslg.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+folder_path=/home/dawei/repo/r3live
+
 # add --network=host if needed
-docker run -it --name r3live \
+docker run -it --rm --name r3live \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v /mnt/wslg:/mnt/wslg \
     -v /usr/lib/wsl:/usr/lib/wsl \
@@ -10,5 +12,7 @@ docker run -it --name r3live \
     -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
     -e PULSE_SERVER=$PULSE_SERVER \
     -e NVIDIA_VISIBLE_DEVICES=all -e NVIDIA_DRIVER_CAPABILITIES=graphics,utility,compute \
-    -v /mnt/c/Users/Public/rosbags/r3live_rosbags:/data \
+    -v /mnt/c/Users/Public/rosbags/r3live_rosbags:/rosbags \
+    -v /mnt/c/Users/Public/wsl_data:/data \
+    -v $folder_path:/catkin_ws/src/r3live \
     --gpus all r3live:noetic bash
