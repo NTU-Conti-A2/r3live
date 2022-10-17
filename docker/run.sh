0af789933d5b0a8a29064a9f16eca45a5fbaadcf
pokealimit <pokealimit@gmail.com>
2022-10-17 12:08:35 +0800

    edited script for easier mount local folder

--- docker/run.sh
+++ docker/run.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
+
+R3LIVE_FOLDER=/home/dawei/repo/r3live
+ROSBAG_FOLDER=/mnt/c/Users/Public/rosbags/r3live_rosbags
+OUTPUT_FOLDER=/mnt/c/Users/Public/wsl_data
+
 # Need to expose host linux x11 server for GUI to be displayed on host (unsafe if on shared system. comment out and use xauth is needed)
 xhost +local:root
 
 # add --network=host if needed
-docker run -itd --name r3live \
+docker run -it --rm --name r3live \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
     -e NVIDIA_VISIBLE_DEVICES=all -e NVIDIA_DRIVER_CAPABILITIES=graphics,utility,compute \
+    -v $ROSBAG_FOLDER:/rosbags \
+    -v $OUTPUT_FOLDER:/data \
+    -v $R3LIVE_FOLDER:/catkin_ws/src/r3live \
     --gpus all r3live:noetic bash
--- docker/run_wslg.sh
+++ docker/run_wslg.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-folder_path=/home/dawei/repo/r3live
+R3LIVE_FOLDER=/home/dawei/repo/r3live
+ROSBAG_FOLDER=/mnt/c/Users/Public/rosbags/r3live_rosbags
+OUTPUT_FOLDER=/mnt/c/Users/Public/wsl_data
 
 # add --network=host if needed
 docker run -it --rm --name r3live \
@@ -12,7 +14,7 @@ docker run -it --rm --name r3live \
     -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
     -e PULSE_SERVER=$PULSE_SERVER \
     -e NVIDIA_VISIBLE_DEVICES=all -e NVIDIA_DRIVER_CAPABILITIES=graphics,utility,compute \
-    -v /mnt/c/Users/Public/rosbags/r3live_rosbags:/rosbags \
-    -v /mnt/c/Users/Public/wsl_data:/data \
-    -v $folder_path:/catkin_ws/src/r3live \
+    -v $ROSBAG_FOLDER:/rosbags \
+    -v $OUTPUT_FOLDER:/data \
+    -v $R3LIVE_FOLDER:/catkin_ws/src/r3live \
     --gpus all r3live:noetic bash
