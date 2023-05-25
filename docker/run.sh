beb494571c02f33c256a848686360f18db379e0e
pokealimit <pokealimit@gmail.com>
2023-05-25 15:12:55 +0800

    changes to allow dynamic uid in container

--- docker/run.sh
+++ docker/run.sh
@@ -4,16 +4,14 @@ R3LIVE_FOLDER=/home/dawei/repo/r3live
 ROSBAG_FOLDER=/mnt/c/Users/Public/rosbags/r3live_rosbags
 OUTPUT_FOLDER=/mnt/c/Users/Public/wsl_data
 
-# Need to expose host linux x11 server for GUI to be displayed on host (unsafe if on shared system. comment out and use xauth is needed)
-xhost +local:root
-
 # add --network=host if needed
 docker run -it --rm --name r3live \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
     -e NVIDIA_VISIBLE_DEVICES=all -e NVIDIA_DRIVER_CAPABILITIES=graphics,utility,compute \
-    -v $ROSBAG_FOLDER:/rosbags \
-    -v $OUTPUT_FOLDER:/data \
-    -v $R3LIVE_FOLDER:/catkin_ws/src/r3live \
-    --gpus all r3live:noetic bash
+    -u $(id -u):$(id -g) \
+    -v $ROSBAG_FOLDER:/home/user/rosbags \
+    -v $OUTPUT_FOLDER:/home/user/r3live_output \
+    -v $R3LIVE_FOLDER:/home/user/catkin_ws/src/r3live \
+    --gpus all r3live:noetic zsh
--- docker/run_wslg.sh
+++ docker/run_wslg.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 R3LIVE_FOLDER=/home/dawei/repo/r3live
-ROSBAG_FOLDER=/mnt/c/Users/Public/rosbags/r3live_rosbags
+ROSBAG_FOLDER=/mnt/c/Users/Public/rosbags
 OUTPUT_FOLDER=/mnt/c/Users/Public/wsl_data
 
 # add --network=host if needed
 docker run -it --rm --name r3live \
+    --network=host \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v /mnt/wslg:/mnt/wslg \
     -v /usr/lib/wsl:/usr/lib/wsl \
@@ -14,7 +15,8 @@ docker run -it --rm --name r3live \
     -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
     -e PULSE_SERVER=$PULSE_SERVER \
     -e NVIDIA_VISIBLE_DEVICES=all -e NVIDIA_DRIVER_CAPABILITIES=graphics,utility,compute \
-    -v $ROSBAG_FOLDER:/rosbags \
-    -v $OUTPUT_FOLDER:/data \
-    -v $R3LIVE_FOLDER:/catkin_ws/src/r3live \
-    --gpus all r3live:noetic bash
+    -v $ROSBAG_FOLDER:/home/user/rosbags \
+    -v $OUTPUT_FOLDER:/home/user/r3live_output \
+    -v $R3LIVE_FOLDER:/home/user/catkin_ws/src/r3live \
+    -u $(id -u):$(id -g) \
+    --gpus all r3live:noetic zsh
